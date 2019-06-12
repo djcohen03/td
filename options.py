02ccd7d2948931dd32246e059f085c7df1f2d868
David Cohen <david.cohen@haloinvesting.com>
2019-06-12 09:35:25 -0500

    Some small model and parsing changes

--- options.py
+++ options.py
@@ -23,7 +23,6 @@ class Helpers(object):
                 strike=strike,
                 expiration=expiration,
             )
-            print 'Created: %s' % option
             return option
 
 class OptionsDataClient(object):
@@ -45,81 +44,95 @@ class OptionsDataClient(object):
         tradable = session.query(Tradable).filter_by(name=name).first()
         response = self.tdclient.optionschain(tradable.name)
 
-        # Gather some base values:
-        interest = response['interestRate']
-        price = response['underlyingPrice']
-
-        # Parse the Calls and Puts in the Response:
-        self._parse(response['callExpDateMap'], tradable, price, interest)
-        self._parse(response['putExpDateMap'], tradable, price, interest)
+        self._parse(response, tradable)
 
         print 'Finished Fetching %s Options Data In %.2fs' % (tradable, time.time() - start)
 
-    def _parse(self, datemap, tradable, underlyingprice, interest):
+    def _parse(self, data, tradable):
         ''' Parse the options chain data and insert in into the Database
         '''
+
+        # Gather some constant values:
+        riskfree = data['interestRate']
+        underlying = data['underlyingPrice']
         now = datetime.datetime.now()
-        bulksave = []
-        for datestr in datemap:
-            expdate, _ = datestr.split(':')
-            expiration = datetime.datetime.strptime(expdate, '%Y-%m-%d').date()
 
-            for strikestr in datemap[datestr]:
-                # Get this Options Data:
-                strike = float(strikestr)
-                data = datemap[datestr][strikestr][0]
+        # Parse the Calls and Puts in the Response:
+        calls = data['callExpDateMap']
+        puts = data['putExpDateMap']
 
-                # Get or Create the Option:
-                option = Helpers.getoption(
-                    tradable=tradable,
-                    expiration=expiration,
-                    type=data['putCall'],
-                    description=data['description'],
-                    symbol=data['symbol'],
-                    exchange=data['exchangeName'],
-                    expirationtype=data['expirationType'],
-                    strike=strike,
-                )
-                optiondata = OptionData(
-                    ask=data['ask'],
-                    asksize=data['askSize'],
-                    bid=data['bid'],
-                    bidsize=data['bidSize'],
-                    close=data['closePrice'],
-                    dte=data['daysToExpiration'],
-                    delta=data['delta'],
-                    gamma=data['gamma'],
-                    low=data['lowPrice'],
-                    high=data['highPrice'],
-                    itm=data['inTheMoney'],
-                    last=data['last'],
-                    lastsize=data['lastSize'],
-                    mark=data['mark'],
-                    markchange=data['markChange'],
-                    rho=data['rho'],
-                    theoreticalvalue=data['theoreticalOptionValue'],
-                    theoreticalvol=data['theoreticalVolatility'],
-                    theta=data['theta'],
-                    timevalue=data['timeValue'],
-                    volume=data['totalVolume'],
-                    vega=data['vega'],
-                    volatility=data['volatility'],
-                    option=option,
-                    time=now,
-                )
-                bulksave.append(option)
-                bulksave.append(optiondata)
-                print 'Created: %s' % optiondata
+        # Loop Through The Entire Options Chain:
+        alloptions = []
+        alloptionsdata = []
+        for datemap, calltype in [(calls, 'Calls'), (puts, 'Puts')]:
+            for datestr in datemap:
+                # Parse the Date String into a datetime object:
+                expdate, _ = datestr.split(':')
+                expiration = datetime.datetime.strptime(expdate, '%Y-%m-%d').date()
 
-        print 'Saving All Data For %s...' % tradable
-        session.add_all(bulksave)
+                print 'Parsing %s %s for: %s..' % (expdate, calltype, tradable)
+
+                # Loop Through All Strike Prices for the given expiration Date:
+                for strikestr in datemap[datestr]:
+                    # Get this Options Data:
+                    strike = float(strikestr)
+                    data = datemap[datestr][strikestr][0]
+
+                    # Get or Create the Option:
+                    option = Helpers.getoption(
+                        tradable=tradable,
+                        expiration=expiration,
+                        type=data['putCall'],
+                        description=data['description'],
+                        symbol=data['symbol'],
+                        exchange=data['exchangeName'],
+                        expirationtype=data['expirationType'],
+                        strike=strike,
+                    )
+                    # Create & Save a new OptionsData row instance:
+                    optiondata = OptionData(
+                        ask=data['ask'],
+                        asksize=data['askSize'],
+                        bid=data['bid'],
+                        bidsize=data['bidSize'],
+                        close=data['closePrice'],
+                        dte=data['daysToExpiration'],
+                        delta=data['delta'],
+                        gamma=data['gamma'],
+                        low=data['lowPrice'],
+                        high=data['highPrice'],
+                        itm=data['inTheMoney'],
+                        last=data['last'],
+                        lastsize=data['lastSize'],
+                        mark=data['mark'],
+                        markchange=data['markChange'],
+                        rho=data['rho'],
+                        theovalue=data['theoreticalOptionValue'],
+                        theovol=data['theoreticalVolatility'],
+                        theta=data['theta'],
+                        timevalue=data['timeValue'],
+                        volume=data['totalVolume'],
+                        vega=data['vega'],
+                        volatility=data['volatility'],
+                        option=option,
+                        time=now,
+                        riskfree=riskfree,
+                        underlying=underlying,
+                    )
+                    alloptions.append(option)
+                    alloptionsdata.append(optiondata)
+
+        print 'Saving %s New Options Data Instances For %s...' % (len(alloptionsdata), tradable)
+        session.add_all(alloptions)
+        session.add_all(alloptionsdata)
         session.commit()
 
 
-
-
 if __name__ == '__main__':
-    tradables = session.query(Tradable).filter_by(name='SPY').all()
     client = OptionsDataClient(tdtoken.token, 'DJCOHEN0115')
-    for tradable in tradables:
-        client.fetch(tradable.name)
+    tradables = session.query(Tradable).filter_by(name='SPY').all()
+    while True:
+        client.authenticate()
+        for tradable in tradables:
+            client.fetch(tradable.name)
+        time.sleep(60 * 90)
